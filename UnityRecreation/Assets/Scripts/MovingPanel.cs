5c99075363eba732a5359a72686b6953573f0298
Carter Ivancic <carterivancic@gmail.com>
2020-12-19 21:04:35 -0500

    Placing sprites.

---
+++ UnityRecreation/Assets/Scripts/MovingPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingPanel : MonoBehaviour
+{
+    public Rect panel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<RectTransform>().transform.position.x < 1200 && this.GetComponent<RectTransform>().transform.position.x > 800 
+            && this.GetComponent<RectTransform>().transform.position.y < 650 && this.GetComponent<RectTransform>().transform.position.y > 350)
+        {
+            UpdatePos();
+        }
+        Debug.Log(this.GetComponent<RectTransform>().transform.position);
+    }
+
+    void UpdatePos()
+    {
+        transform.position = -Input.mousePosition;
+    }
+}
